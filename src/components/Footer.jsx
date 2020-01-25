5d1e57de43efc59a08f47143ef4bb9266f9c9e5b
Khristian Brooks <kh.brooks02@gmail.com>
2020-01-25 02:33:41 -0500

    finished design for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import "./Footer.css";
 import logo from "../images/IMG_9879.JPG";
 import instagram from "../images/instagram.png";
 import facebook from "../images/facebook.png";
 import twitter from "../images/twitter.png";
+import home from "../images/home.png";
+import email from "../images/email.png";
+import phone from "../images/phone.png";
 
 export default function Footer() {
 	return (
@@ -21,7 +24,7 @@ export default function Footer() {
 					<Row className='text-center'>
 						<Col>
 							<h6 className='text-uppercase mb-4 font-weight-bold'>
-								<img src={logo} height={75} />
+								<img alt='' src={logo} height={75} />
 							</h6>
 							<p>
 								We strive to provide support to enable individuals to achieve
@@ -32,58 +35,51 @@ export default function Footer() {
 						<Col>
 							<h6 className='text-uppercase mb-4 font-weight-bold'>Contact</h6>
 							<p>
-								<i className='fas fa-home mr-3'></i> New York, NY 10012, US
+								<img alt='' src={home} height={15} /> Brooklyn, NY, US
 							</p>
 							<p>
-								<i className='fas fa-envelope mr-3'></i> info@gmail.com
+								<img alt='' src={email} height={15} />{" "}
+								info@theaparafoundation.com
 							</p>
 							<p>
-								<i className='fas fa-phone mr-3'></i> + 01 234 567 88
-							</p>
-							<p>
-								<i className='fas fa-print mr-3'></i> + 01 234 567 89
+								<img alt='' src={phone} height={15} /> + 01 234 567 88
 							</p>
 						</Col>
 
 						<Col>
-							<div className='col-md-3 col-lg-2 col-xl-2 mx-auto mt-3'>
-								<h6 className='text-uppercase mb-4 font-weight-bold'>
-									Follow Us On Social Media
-								</h6>
-								<ul className='list-unstyled list-inline'>
-									<li className='list-inline-item'>
-										<a className='btn-floating btn-sm rgba-white-slight mx-1'>
-											<img
-												src={facebook}
-												height={25}
-												className='fab fa-facebook-f'
-											/>
-										</a>
-									</li>
-									<li className='list-inline-item'>
-										<a className='btn-floating btn-sm rgba-white-slight mx-1'>
-											<img
-												src={twitter}
-												height={25}
-												className='fab fa-twitter'
-											/>
-										</a>
-									</li>
-									<li className='list-inline-item'>
-										<a className='btn-floating btn-sm rgba-white-slight mx-1'>
-											<img
-												src={instagram}
-												height={25}
-												className='fab fa-google-plus-g'
-											/>
-										</a>
-									</li>
-								</ul>
-							</div>
+							<h6 className='text-uppercase mb-4 font-weight-bold'>
+								Follow Us On Social Media
+							</h6>
+							<ul className='list-unstyled list-inline'>
+								<li className='list-inline-item'>
+									<img
+										src={facebook}
+										alt=''
+										height={25}
+										className='fab fa-facebook-f'
+									/>
+								</li>
+								<li className='list-inline-item'>
+									<img
+										alt=''
+										src={twitter}
+										height={25}
+										className='fab fa-twitter'
+									/>
+								</li>
+								<li className='list-inline-item'>
+									<img
+										src={instagram}
+										alt=''
+										height={25}
+										className='fab fa-google-plus-g'
+									/>
+								</li>
+							</ul>
 						</Col>
 					</Row>
 
-					<hr />
+					<hr className='separator' />
 
 					<Row className='align-items-center'>
 						<div>
