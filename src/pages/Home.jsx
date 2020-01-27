7618c098caafc0573a843285598c011ee5f28d25
Khristian Brooks <kh.brooks02@gmail.com>
2020-01-26 20:06:30 -0500

    link action to contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,43 +67,59 @@ export default function Home() {
 				<Container>
 					<h1>Get Involved</h1>
 					<Row>
-						<Col className='action'>
-							<Image src={donate} width={100} />
-							<h3> Send Donation</h3>
-							<p>
-								Give online, and your contribution will make a difference. Your
-								donations will go to help furthering our foundation’s goals and
-								visions.
-							</p>
+						<Col>
+							<a href='/contact'>
+								<div className='action'>
+									<Image src={donate} width={100} />
+									<h3> Send Donation</h3>
+									<p>
+										Give online, and your contribution will make a difference.
+										Your donations will go to help furthering our foundation’s
+										goals and visions.
+									</p>
+								</div>
+							</a>
 						</Col>
-						<Col className='action'>
-							<Image src={volunteer} width={100} />
-							<h3>Become a Volunteer</h3>
-							<p>
-								You can get involved today by becoming a Volunteer. Sign up and
-								you will be joining a group of change-makers, a network strong
-								enough to impact positive change.
-							</p>
+						<Col>
+							<a href='/contact'>
+								<div className='action'>
+									<Image src={volunteer} width={100} />
+									<h3>Become a Volunteer</h3>
+									<p>
+										You can get involved today by becoming a Volunteer. Sign up
+										and you will be joining a group of change-makers, a network
+										strong enough to impact positive change.
+									</p>
+								</div>
+							</a>
 						</Col>
-						<Col className='action'>
-							<Image src={scholarship} width={100} />
-							<h3>Give Scholarship</h3>
-							<p>
-								Your gift will help equip children in need with necessary
-								resources, training and education while offering the promise of
-								a brighter future. You can make a difference today by signing
-								up.
-							</p>
+						<Col>
+							<a href='/contact'>
+								<div className='action'>
+									<Image src={scholarship} width={100} />
+									<h3>Give Scholarship</h3>
+									<p>
+										Your gift will help equip children in need with necessary
+										resources, training and education while offering the promise
+										of a brighter future. You can make a difference today by
+										signing up.
+									</p>
+								</div>
+							</a>
 						</Col>
-						<Col className='action'>
-							<Image src={project} width={100} />
-							<h3>Sponsor a Project</h3>
-							<p>
-								The APARA Foundation has many projects that need your support.
-								Whether that be our Sickle Cell Walk, Baskets for Boomie, Puzzle
-								Pieces, or other projects, your contribution will ensure its
-								progression.
-							</p>
+						<Col>
+							<a href='/contact'>
+								<div className='action'>
+									<Image src={project} width={100} />
+									<h3>Sponsor a Project</h3>
+									<p>
+										The APARA Foundation has many projects that need your
+										support. Whether that be our Sickle Cell Walk, Baskets for
+										Boomie, Puzzle Pieces, or other projects, your contribution
+										will ensure its progression.
+									</p>
+								</div>
+							</a>
 						</Col>
 					</Row>
 				</Container>
