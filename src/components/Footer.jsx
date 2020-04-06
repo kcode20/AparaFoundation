6cf438613a117fed8baf3fbaef70523dff8e0cad
Khristian Brooks <kh.brooks02@gmail.com>
2020-04-06 11:22:21 -0400

    add gtm script, correct email in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,7 +39,7 @@ export default function Footer() {
 							</p>
 							<p>
 								<img alt='' src={email} height={15} />
-								info@theaparafoundation.com
+								info@theaparafoundation.org
 							</p>
 						</Col>
 
