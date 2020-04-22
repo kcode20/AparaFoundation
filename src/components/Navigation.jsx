c7e39dd596b098d2b8a471cc2cbe38995f66c298
Khristian Brooks <kh.brooks02@gmail.com>
2020-04-21 20:41:35 -0400

    mobile friendly navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import logo from "../images/transparent-logo.png";
-import "./Navigation.css";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../images/transparent-logo.png';
+import './Navigation.css';
 
 export default function App() {
 	return (
 		<>
-			<Navbar bg='light' variant='light' fixed='top'>
+			<Navbar bg='light' variant='light' fixed='top' expand='lg'>
 				<Navbar.Brand href='/'>
 					<img src={logo} height={100} alt='Apara Foundation' />
 				</Navbar.Brand>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav className='mr-auto'></Nav>
-					<Nav>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='mr-auto'>
 						<Nav.Link className='linkStyle' href='/'>
 							Home
 						</Nav.Link>
