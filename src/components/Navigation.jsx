61a042af3ca35571708c432c63167ce845c18471
Khristian Brooks <kh.brooks02@gmail.com>
2020-05-09 13:42:03 -0400

    pull content of navbar right

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ export default function App() {
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav className='mr-auto'>
+					<Nav className='ml-auto'>
 						<Nav.Link className='linkStyle' href='/'>
 							Home
 						</Nav.Link>
